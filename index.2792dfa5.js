647110265a6a97a6b35999bb81e807a550b520c0
Matteo Micari <megamatteo27@gmail.com>
2024-12-06 22:59:18 +0100

    Updates

--- index.2792dfa5.js
+++
@@ -1,2 +0,0 @@
-window.addEventListener("keydown",function(n){switch(n.key){case"w":case"s":case"a":case"d":break;case"z":e.controls.lock()}},!1),window.addEventListener("keyup",function(e){e.key},!1);var e=new Game;
-//# sourceMappingURL=index.2792dfa5.js.map
---
+++ index.3e8ca064.js
@@ -0,0 +1,2 @@
+window.addEventListener("keydown",function(e){switch(e.key){case"w":case"s":case"a":case"d":break;case"z":game.controls.lock()}},!1),window.addEventListener("keyup",function(e){e.key},!1),window.game=new Game;
+//# sourceMappingURL=index.3e8ca064.js.map
